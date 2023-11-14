9ecca18f43e91c0bc8d5c2de797078604010f6a9
shodrig0 <138055427+shodrig0@users.noreply.github.com>
2023-11-14 16:05:28 -0300

    actualizacion nombre archivo prog ppal

--- programaVillablancaAlmeiraGarcia.php
+++ programaVillablancaAlmeiraGarcia.php
@@ -9,7 +9,9 @@ include_once("wordix.php");
 
 /* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
 /* ****COMPLETAR***** */
-
+/*  Rodrigo Villablanca - Legajo FAI-5097 - email: rodrialvillablanca@gmail.com - Github: shodrig0
+    Paola Fernanda Garcia Romero - Legajo FAI4387 - mail: paolagarcianqn@gmail.com - Github: PaoGarciaRF
+    Ilel Luciana Almeira - Legajo FAI 4914 - email: ilelalmeira@gmail.com - Github: ilelalmeira
 
 /**************************************/
 /***** DEFINICION DE FUNCIONES ********/
