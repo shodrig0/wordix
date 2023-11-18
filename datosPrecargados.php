6ca7ac4e5c9cbc47b55c5675988a162c38335489
shodrig0 <138055427+shodrig0@users.noreply.github.com>
2023-11-18 03:12:17 -0300

    actulización

--- datosPrecargados.php
+++ datosPrecargados.php
@@ -3,8 +3,33 @@
 /** Estructura de datos.
  *  Necesaria para los datos precargados.
  */
+/**
+ * @param array $coleccionPartidasPrecargadas
+ * @param array $cargarPartidasPrecargadas
+ * @return int
+ */
 
-     
+function cargarPartidasPrecargadas()
+{
+    $coleccionPartidasPrecargadas[0] = ["palabraWordix" => "QUESO", "jugador" => "majo", "intentos" => 0, "puntaje" => 0];
+    $coleccionPartidasPrecargadas[1] = ["palabraWordix" => "TRONO", "jugador" => "shodrig0", "intentos" => 3, "puntaje" => 15];
+    $coleccionPartidasPrecargadas[2] = ["palabraWordix" => "BOTAS", "jugador" => "il3l", "intentos" => 4, "puntaje" => 13];
+    $coleccionPartidasPrecargadas[3] = ["palabraWordix" => "NAVES", "jugador" => "facuu", "intentos" => 6, "puntaje" => 12];
+    $coleccionPartidasPrecargadas[4] = ["palabraWordix" => "HUEVO", "jugador" => "majo", "intentos" => 5, "puntaje" => 9];
+    $coleccionPartidasPrecargadas[5] = ["palabraWordix" => "VERDE", "jugador" => "P40oo", "intentos" => 5, "puntaje" => 12];
+    $coleccionPartidasPrecargadas[6] = ["palabraWordix" => "YUYOS", "jugador" => "shodrig0", "intentos" => 0, "puntaje" => 0];
+    $coleccionPartidasPrecargadas[7] = ["palabraWordix" => "REINA", "jugador" => "albap", "intentos" => 3, "puntaje" => 13];
+    $coleccionPartidasPrecargadas[8] = ["palabraWordix" => "YUYOS", "jugador" => "clauu", "intentos" => 1, "puntaje" => 17];
+    $coleccionPartidasPrecargadas[9] = ["palabraWordix" => "MUJER", "jugador" => "il3l", "intentos" => 5, "puntaje" => 11];
+    $coleccionPartidasPrecargadas[10] = ["palabraWordix" => "TIGRE", "jugador" => "facuu", "intentos" => 2, "puntaje" => 15];
+    $coleccionPartidasPrecargadas[11] = ["palabraWordix" => "DAMAS", "jugador" => "P40oo", "intentos" => 0, "puntaje" => 0];
+    $coleccionPartidasPrecargadas[12] = ["palabraWordix" => "PIANO", "jugador" => "albap", "intentos" => 5, "puntaje" => 11];
+    $coleccionPartidasPrecargadas[13] = ["palabraWordix" => "REINA", "jugador" => "clauu", "intentos" => 2, "puntaje" => 14];
+    $coleccionPartidasPrecargadas[14] = ["palabraWordix" => "PISOS", "jugador" => "il3l", "intentos" => 0, "puntaje" => 0];
+
+    return $coleccionPartidasPrecargadas;
+}
+
 $jugadorDatosPrecargados[] = [
     "jugador" => "",
     "partidas" => 0,
