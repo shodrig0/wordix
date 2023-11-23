8638e69531c62ffb62079bf0e8bbfec1f256542d
ilelalmeira <ilel.almeira@est.fi.uncoma.edu.ar>
2023-11-22 21:40:09 -0300

    funcion cantidad de partidas

--- programaVillablancaAlmeiraGarcia.php
+++ programaVillablancaAlmeiraGarcia.php
@@ -79,6 +79,11 @@ do {
                         //INTENTANDO QUE SI EL INTENTO DE ADIVINAR FUE UNO DIGA INTENTO Y NO INTENTOS.. 
             }
             break;
+        case 5: 
+           echo "Ingrese el nombre de un jugador","\n";
+           $jugador = trim (fgets(STDIN));
+           $partidasJugadas = cantidadDePartidas ($jugador, $coleccionPartidasJugadas);
+           echo $partidasJugadas;
 
         case 8:
 
